5bd6f3b4179b6bdbb844fe953431e0a418dd76de
ruslan123411 <63186285+ruslan123411@users.noreply.github.com>
2020-04-05 12:03:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("text");
+var myVar;
+let myVar1;
+myVar1=14;
+console.log("Возраст: ",myVar1);
+let myName="руслан";
+let myVar2= true;
+myVar2=false;
+console.log(myName);
+console.log(myVar2);
